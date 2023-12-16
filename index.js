396227cc7fa51bae2ac324cb374ee46fb36d06dc
yeswanSankar <134676707+yeswanSankar@users.noreply.github.com>
2023-12-16 12:53:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-var http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('You are  in AWS  Cloud. THis is TECHESSAY, Please share and Subscribe and like my video'); //write a response to the client
-  res.end(); //end the response
-}).listen(4000); //the server object listens on port 4000
+app.get('/', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+                body {
+                    background-color: purple;
+                    font-style: italic;
+                    font-family: 'Arial', sans-serif;
+                    color: white;
+                    text-align: center;
+                    padding: 50px;
+                }
+            </style>
+            <title>Welcome to Cloud aseem Youtube channel ,Please share & subscribe , Thank you </title>
+        </head>
+        <body>
+            <h1>Welcome to Cloud aseem Youtube channel ,Please share & subscribe , Thank you very much for 200 subscribers , keep loving , thankyou <3 </h1>
+            <p>Don't forget to subscribe & share with friends</p>
+        </body>
+        </html>
+    `);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
